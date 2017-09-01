7dd8432e87fd4ca25f58d56fb83d0961a616596b
Danny123456789121 <kurthdaniel@web.de>
2017-09-01 17:36:03 +0200

    Erste Buttons
    -Mining Tool hinzugefügt
    
    -Gold Display hinzugefügt
    
    -Buttons für den Shop, bzw. powerup's eingerichtet

---
+++ Cookie Clicker 2.0/Assets/Scripts/Click.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Click : MonoBehaviour
+{
+    public Text goldDisplay;
+    public int gold = 0;
+    public int goldperclick = 1;
+
+
+    private void Update()
+    {
+        goldDisplay.text = "Gold : " + gold;
+
+    }
+
+    public void Clicked ()
+    {
+        gold += goldperclick;
+
+    }
+
+}
